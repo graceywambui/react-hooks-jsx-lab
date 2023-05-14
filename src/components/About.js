77fb18d6ccba81e3c9e10f83e77e0a22e58bc643
grace njeri wambui <gracenjeriwambui00@gmail.com>
2023-05-14 20:02:57 +0300

    JSX LAB

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { image } from "../data/data";
+import {image} from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div>About
+    <h2>About Me</h2>
+    <p>you wont break my soul</p>
+    <img src={image} alt='I made this' />
+  </div>;
 }
 
 export default About;
+
